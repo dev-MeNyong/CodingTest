97a9e1ee8d72aab5f24603dd14a5aaaad5de1532
dev_me_nyong <80152716+dev-MeNyong@users.noreply.github.com>
2023-02-14 23:29:30 +0900

    다음에 올 숫자

---
+++ Programmers/programmers02.js
@@ -0,0 +1,45 @@
+var commons =[1,2,3,4];
+var v = [2,4,8];
+//등차수열
+
+function solution(common) {
+  const value=common[1]-common[0];
+  const divide_val=common[1] / common[0];
+
+  let len = common.length;
+
+  let cnt = 0;
+  for(let i = 1; i <  len - 1 ; i++){
+    if(common[i+1] - common[i] === value){ //등차수열
+        cnt = cnt + 1;
+        if(cnt == len - 2){
+            answer = common[len - 1]+value;
+            cnt = 0;
+            return answer;
+        }
+    }
+    else if(common[i+1]/common[i]===divide_val){ //등비수열
+        cnt = cnt + 1;
+        if(cnt == len - 2){
+            answer = common[len - 1] * divide_val;
+            cnt = 0;
+            return answer;
+        }
+    }
+  }
+  return answer;
+}
+
+console.log(solution(commons));
+console.log(solution(v));
+/*
+간단하면서도 굉장한 풀이,,,
+function solution(common) {
+    if ((common[1]-common[0])==(common[2]-common[1])){
+        return common.pop() + common[1] - common[0];
+    }
+    else{
+        return common.pop()*common[1]/common[0];
+    }
+}
+*/
